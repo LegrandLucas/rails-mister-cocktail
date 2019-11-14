0aac9a50577602b72e3b1d2d6b9e5d32cdfd877f
Lucas Legrand <legrand.lucas0@gmail.com>
2019-11-14 14:29:01 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+data = open(url).read
+ingredients = JSON.parse(data)
+
+drinks = ingredients['drinks']
+puts 'adding ingredients'
+drinks.each do |drink|
+  ingredient = Ingredient.new(name: drink['strIngredient1'])
+  ingredient.save
+end
+
+puts 'Seed in progress...'
